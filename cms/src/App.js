9ac47c7e1cb50b0d337f694921561b43f8394cf0
pneum4 <ysyoung4@naver.com>
2022-07-06 16:16:44 +0900

    LandingPage 추가

--- cms/src/App.js
+++ cms/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import LandingPage from "./views/LandingPage/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <LandingPage></LandingPage>
-    </>
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
   );
 }
 
--- cms/src/views/LandingPage/LandingPage.js
+++ cms/src/views/LandingPage/LandingPage.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.scss";
 
 const LandingPage = () => {
+  const [showPW, setshowPW] = useState(false);
+
+  const onEye = (e) => {
+    switch (e.type) {
+      case "mousedown":
+        setshowPW(true);
+        break;
+      case "mouseup":
+        setshowPW(false);
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <div className="body">
       <div className="login-container">
@@ -10,7 +24,15 @@ const LandingPage = () => {
           <img src="icons/lock.png" alt="LOCK_IMG" />
         </div>
         <input type="text" />
-        <input type="password" />
+        <div className="password-box">
+          {showPW ? <input type="text" /> : <input type="password" />}
+          <img
+            src="icons/eye.png"
+            alt="LOCK_IMG"
+            onMouseDown={onEye}
+            onMouseUp={onEye}
+          />
+        </div>
         <span>Login</span>
         <div className="register-area">
           <span>문의하기</span>
